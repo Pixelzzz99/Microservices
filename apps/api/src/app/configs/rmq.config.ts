33ff3b1848a6f0dff96f3acbb732ce8f130002f6
Sherzik <sherzik99@gmail.com>
2023-10-24 20:32:30 +0500

    connect to accoutn

---
+++ apps/api/src/app/configs/rmq.config.ts
@@ -0,0 +1,19 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { IRMQServiceAsyncOptions } from 'nestjs-rmq';
+
+export const getRMQConfig = (): IRMQServiceAsyncOptions => ({
+  inject: [ConfigService],
+  imports: [ConfigModule],
+  useFactory: (configService: ConfigService) => ({
+    exchangeName: configService.get('ARMQ_EXCHANGE') ?? '',
+    connections: [
+      {
+        login: configService.get('ARMQ_USER') ?? '',
+        password: configService.get('ARMQ_PASSWORD') ?? '',
+        host: configService.get('ARMQ_HOST') ?? '',
+      },
+    ],
+    prefetchCount: 32,
+    serviceName: 'purple-service',
+  }),
+});
